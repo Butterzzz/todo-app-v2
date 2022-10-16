894dd30f38f153edcbcf168416efedecd98fead6
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-17 02:26:38 +0900

    feat: add Todos using TodoForm

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState(['first', 'second']);
+  const [todos, setTodos] = useState([]);
+
+  const addTodoHandler = (text) => {
+    setTodos([...todos, text])
+  }
 
   return (
     <div className="App">
       <h1>Todo App</h1>
-      <TodoForm />
+      <TodoForm addTodo={addTodoHandler} />
       <TodoList todos={todos} />
     </div>
   );
--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,7 +1,21 @@
-function TodoForm() {
+import { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+    const [text, setText] = useState('');
+
+    const onSubmitHandler = (evt) => {
+        evt.preventDefault();
+        addTodo(text);
+        setText('');
+    }
+
     return (
-        <form>
-            <input placeholder="Новая задача" />
+        <form onSubmit={onSubmitHandler}>
+            <input
+                placeholder="Новая задача"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <button type="submit">Создать</button>
         </form>
 
