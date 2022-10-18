519264fe598bb7a75756aeaca2e75ca15a380e35
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-18 21:48:21 +0900

    refactor: change array to array of objects

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -5,7 +5,7 @@ function TodoList({ todos, deleteTodo }) {
     return (
         <div className={styles.todoListContainer}>
             {!todos.length && <h2>Список задач пустой</h2>}
-            {todos.map((todo, index) => <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo}/>)}
+            {todos.map((todo) => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo}/>)}
         </div>
     )
 }
