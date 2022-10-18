9f1d3888d8bda4c76dbad110be05791964ae8eb9
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-19 01:18:20 +0900

    styles: for emoval of individual tasks

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -3,14 +3,18 @@ import { RiTodoFill, RiDeleteBin2Line } from "react-icons/ri";
 import { FaCheck } from "react-icons/fa";
 
 
-function Todo({ todo, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleTodo }) {
     return (
-        <div className={styles.todo}>
+        <div className={`${styles.todo} ${todo.isComplited ? styles.completedTodo : ''}`}>
             <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.text}</div>
 
-            <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)} />
-            <FaCheck className={styles.checkIcon} />
+            <RiDeleteBin2Line
+                className={styles.deleteIcon}
+                onClick={() => deleteTodo(todo.id)} />
+            <FaCheck
+                className={styles.checkIcon}
+                onClick={() => toggleTodo(todo.id)} />
         </div>
     )
 }
